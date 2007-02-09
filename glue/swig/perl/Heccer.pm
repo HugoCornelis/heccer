004419115658fe025b3e1b981d6025fc67ef656c
Hugo Cornelis <hugo.cornelis@gmail.com>
2007-02-09 21:46:00 +0000

    1. Updated .mtn-ignore after previous checkin. 2. Added Heccer.pm intermediary level Swig interface. 3. Fixed distribution of Swig interface. 4. Added tests for intermediary level Swig interface.

---
+++ glue/swig/perl/Heccer.pm
@@ -0,0 +1,199 @@
+#!/usr/bin/perl -w
+#!/usr/bin/perl -w -d:ptkdb
+#
+
+package Heccer;
+
+
+package Heccer::Component;
+
+
+use SwiggableHeccer;
+
+
+#t I would like to be able to use MATH_TYPE_Compartment for the type_numbers here.
+#t
+#t MATH_TYPE_Compartment because of strict subs
+#t SwiggableHeccer::MATH_TYPE_Compartment because of strict subs
+#t Heccerc::MATH_TYPE_Compartment fails because it returns 0 instead of 1.
+
+my $heccer_mapping
+    = {
+       compartment => {
+		       type_number => 1,
+		       internal_name => 'Compartment',
+		      },
+       intermediary => {
+			translators => {
+					compartments => {
+							 target => 'pcomp',
+							 convertor =>
+							 sub
+							 {
+							     my $target = shift;
+
+							     my $value = shift;
+
+							     my $result = SwiggableHeccer::comp_array($#$value + 1);
+
+							     foreach my $compartment_index (0 .. @$value)
+							     {
+								 my $compartment = $value->[$compartment_index];
+
+								 SwiggableHeccer::comp_set($result, $compartment_index, $compartment);
+							     }
+
+							     return $result;
+							 },
+							},
+					comp2mech => {
+						      target => 'piC2m',
+						      convertors =>
+						      sub
+						      {
+							  my $target = shift;
+
+							  my $value = shift;
+
+							  my $result = SwiggableHeccer::int_array($#$value + 1);
+
+							  foreach my $compartment_index (0 .. @$value)
+							  {
+							      my $compartment_start = $value->[$compartment_index];
+
+							      SwiggableHeccer::int_set($result, $compartment_index, $compartment_start);
+							  }
+
+							  return $result;
+						      },
+						     },
+				       },
+			internal_name => 'Intermediary',
+		       },
+      };
+
+
+sub new
+{
+    my $package = shift;
+
+    my $type = shift;
+
+    my $settings = shift;
+
+    if (!exists $heccer_mapping->{$type})
+    {
+	return "$type is not a Heccer::Component";
+    }
+
+    # create the heccer internal object
+
+    my $internal_name = $heccer_mapping->{$type}->{internal_name};
+
+    my $factory_package = "SwiggableHeccer::$internal_name";
+
+    my $self
+	= {
+	   $type => $factory_package->new(),
+	  };
+
+    # never forget to set the heccer type
+
+    my $type_number = $heccer_mapping->{$type}->{type_number};
+
+    if ($type_number)
+    {
+	my $mc = $self->{$type}->swig_mc_get();
+
+	$mc->swig_iType_set($type_number);
+    }
+
+    # just apply all the settings, the missing ones are under
+    # control of heccer
+
+    foreach my $setting (keys %$settings)
+    {
+	# get target name in heccer internal
+
+	my $target = $setting;
+
+	my $value = $settings->{$setting};
+
+	# if there is a translator for this setting
+
+	my $translator = $heccer_mapping->{$type}->{translators}->{$setting};
+
+	if ($translator)
+	{
+	    # translate the target
+
+	    $target = $translator->{$target};
+
+	    # translate the value
+
+	    my $convertor = $translator->{convertor};
+
+	    $value = &$convertor($target, $value);;
+	}
+
+	# set the heccer internal target
+
+	my $subname = "swig_${target}_set";
+
+	$self->{$type}->$subname($value);
+    }
+
+    # return result
+
+    bless $self, $package;
+}
+
+
+#t all the components can be created automatically from the heccer
+#t mapping
+
+package Heccer::Compartment;
+
+sub new
+{
+    my $package = shift;
+
+    my $settings = shift;
+
+    my $result = Heccer::Component->new($package, 'compartment', @_, );
+
+    if (ref $result)
+    {
+	return bless $result, $package;
+    }
+    else
+    {
+	return $result;
+    }
+}
+
+
+package Heccer::Intermediary;
+
+sub new
+{
+    my $package = shift;
+
+    my $settings = shift;
+
+    my $result = Heccer::Component->new($package, 'compartment', @_, );
+
+    if (ref $result)
+    {
+	return bless $result, $package;
+    }
+    else
+    {
+	return $result;
+    }
+}
+
+
+1;
+
+
--- glue/swig/perl/heccer_test.pm
+++ glue/swig/perl/heccer_test.pm
@@ -1,3 +1,4 @@
+#!/usr/bin/perl -w -d:ptkdb
 #!/usr/bin/perl -w
 #!/usr/bin/perl -w -d:ptkdb
 #
@@ -19,6 +20,8 @@ BEGIN
     push @INC, '/usr/local/glue/swig/perl';
 }
 
+
+use Heccer;
 
 use SwiggableHeccer;
 
@@ -70,6 +73,91 @@ use Data::Dumper;
 
     print Dumper(\%{"SwiggableHeccer::ExponentialDecay::"});
 
+}
+
+
+sub c1c2p2
+{
+    print "c1c2p2 bindings\n";
+
+    # construct soma compartment
+
+    my $compartments
+	= [
+	   Heccer::Compartment->new
+	   (
+	    {
+	     dCm => 4.57537e-11,
+	     dEm => -0.08,
+	     dInitVm => -0.068,
+	     dInject => 1e-8,
+	     dRa => 360502,
+	     dRm => 3.58441e+08,
+	     iParent => -1,
+	    },
+	   ),
+	   Heccer::Compartment->new
+	   (
+	    {
+	     dCm => 5.755329373e-12,
+	     dEm => -0.08,
+	     dInitVm => -0.068,
+	     dInject => 0,
+	     dRa => 772813.4375,
+	     dRm => 8.548598272e9,
+	     iParent => 0,
+	    },
+	   ),
+	  ];
+
+    my $intermediary
+	    = Heccer::Intermediary->new
+	      (
+	       {
+		comp2mech => [ 0, 0, -1],
+		compartments => $compartments,
+		iCompartments => 2,
+	       },
+	      );
+
+    # instantiate a heccer with an initialized intermediary
+
+    my $heccer = SwiggableHeccer::HeccerNewP2($intermediary);
+
+    # build indices for optimization
+
+    $heccer->HeccerCompileP2();
+
+    # compile to byte code
+
+    $heccer->HeccerCompileP3();
+
+    # initiate values
+
+    $heccer->HeccerInitiate();
+
+    # initial dump
+
+    $heccer->HeccerDumpV();
+
+    # a couple of times
+
+    foreach (0 .. 2)
+    {
+	# step
+
+	$heccer->HeccerHecc();
+
+	# dump
+
+	print "-------\n";
+
+	print "Iteration $_\n";
+
+	$heccer->HeccerDumpV();
+    }
+
+    print "c1c2p2 bindings: done\n";
 }
 
 
@@ -163,9 +251,99 @@ sub singlep
 }
 
 
-sub c1c2p2
+sub swig_singlep
 {
-    print "c1c2p2 bindings\n";
+    print "swig_singlep bindings\n";
+
+    # construct soma compartment
+
+    my $soma = SwiggableHeccer::Compartment->new();
+
+    my $mc = $soma->swig_mc_get();
+
+
+    #t I would like to be able to use MATH_TYPE_Compartment here.
+    #t
+    #t MATH_TYPE_Compartment because of strict subs
+    #t SwiggableHeccer::MATH_TYPE_Compartment because of strict subs
+    #t Heccerc::MATH_TYPE_Compartment fails because it returns 0 instead of 1.
+
+    $mc->swig_iType_set(1);
+
+    $soma->swig_dCm_set(4.57537e-11);
+    $soma->swig_dEm_set(-0.08);
+    $soma->swig_dInitVm_set(-0.068);
+    $soma->swig_dInject_set(0);
+    $soma->swig_dRa_set(360502);
+    $soma->swig_dRm_set(3.58441e+08);
+    $soma->swig_iParent_set(-1);
+
+    my $intermediary = SwiggableHeccer::Intermediary->new();
+
+    $intermediary->swig_iCompartments_set(1);
+
+    $intermediary->swig_pcomp_set($soma);
+
+    my $pi = $intermediary->swig_piC2m_get();
+
+    my $piC2m = SwiggableHeccer::int_array(2);
+
+    #t get rid of count by implementing a push, needs current.
+
+    my $comp2mech = [ 0, -1];
+
+    my $count = 0;
+
+    map { SwiggableHeccer::int_set($piC2m, $count++, $_) } @$comp2mech;
+
+    # SwiggableHeccer::piC2m_set($intermediary, $piC2m, );
+
+    $intermediary->swig_piC2m_set( $piC2m );
+
+    # instantiate a heccer with an initialized intermediary
+
+    my $heccer = SwiggableHeccer::HeccerNewP2($intermediary);
+
+    # build indices for optimization
+
+    $heccer->HeccerCompileP2();
+
+    # compile to byte code
+
+    $heccer->HeccerCompileP3();
+
+    # initiate values
+
+    $heccer->HeccerInitiate();
+
+    # initial dump
+
+    $heccer->HeccerDumpV();
+
+    # a couple of times
+
+    foreach (0 .. 2)
+    {
+	# step
+
+	$heccer->HeccerHecc();
+
+	# dump
+
+	print "-------\n";
+
+	print "Iteration $_\n";
+
+	$heccer->HeccerDumpV();
+    }
+
+    print "swig_singlep bindings: done\n";
+}
+
+
+sub swig_c1c2p2
+{
+    print "swig_c1c2p2 bindings\n";
 
     # construct soma compartment
 
@@ -277,12 +455,17 @@ sub c1c2p2
 	$heccer->HeccerDumpV();
     }
 
-    print "c1c2p2 bindings: done\n";
+    print "swig_c1c2p2 bindings: done\n";
 }
 
+
+swig_singlep();
+
+swig_c1c2p2();
 
 singlep();
 
 c1c2p2();
+
 
 print "Perl script finished\n";
