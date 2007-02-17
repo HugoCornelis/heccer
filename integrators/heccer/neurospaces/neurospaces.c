67b56c07513dcf16f90f32f15a28f82d2d403673
Hugo Cornelis <hugo.cornelis@gmail.com>
2007-02-17 04:34:40 +0000

    1. Added integration framework, first version.

---
+++ integrators/heccer/neurospaces/neurospaces.c
@@ -0,0 +1,24 @@
+//
+// Heccer : a compartmental solver that implements efficient Crank-Nicolson
+// integration for neuronal models.
+//
+
+//////////////////////////////////////////////////////////////////////////////
+//'
+//' Heccer : testbed C implementation
+//'
+//' Copyright (C) 2006-2007 Hugo Cornelis
+//'
+//' functional ideas ..	Hugo Cornelis, hugo.cornelis@gmail.com
+//'
+//' coding ............	Hugo Cornelis, hugo.cornelis@gmail.com
+//'
+//////////////////////////////////////////////////////////////////////////////
+
+
+int neurospaces(void)
+{
+    return(1);
+}
+
+
