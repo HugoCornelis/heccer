aadd7c02ef0b36389c456c6b25a2ea6e448bf5f7
Hugo Cornelis <hugo.cornelis@gmail.com>
2008-02-24 04:46:24 +0000

    1. Keywords only: build-8

--- heccer.c
+++ heccer.c
@@ -461,7 +461,7 @@ int HeccerError(struct Heccer *pheccer, char *pcContext, char *pcError, ...)
 char * HeccerGetVersion(void)
 {
     // $Format: "    static char *pcVersion=\"${label}\";"$
-    static char *pcVersion="build-6";
+    static char *pcVersion="build-8";
 
     return(pcVersion);
 }
