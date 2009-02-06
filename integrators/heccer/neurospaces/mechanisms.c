57eab0d2f31be75db0fe32d3105e52dbac0da53c
mandorodriguez <mandorodriguez@gmail.com>
2009-02-06 21:16:32 +0000

    BUGFIX: Changed two more occurances of instanceof_equation to instaceof_equation_exponential.

--- integrators/heccer/neurospaces/mechanisms.c
+++ integrators/heccer/neurospaces/mechanisms.c
@@ -3173,7 +3173,7 @@ solver_mathcomponent_processor(struct TreespaceTraversal *ptstr, void *pvUserdat
     else if (instanceof_gate_kinetic(phsle)
 	     || instanceof_h_h_gate(phsle)
 	     || instanceof_concentration_gate_kinetic(phsle)
-	     || instanceof_equation(phsle))
+	     || instanceof_equation_exponential(phsle))
     {
 	//- ok, skip
 
@@ -4173,7 +4173,7 @@ solver_mathcomponent_typer(struct TreespaceTraversal *ptstr, void *pvUserdata)
     else if (instanceof_gate_kinetic(phsle)
 	     || instanceof_h_h_gate(phsle)
 	     || instanceof_concentration_gate_kinetic(phsle)
-	     || instanceof_equation(phsle))
+	     || instanceof_equation_exponential(phsle))
     {
 	//- ok, skip
 
