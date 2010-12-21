d1e564c8acc3005d02f01ff30f5d42c2e3648e49
mandorodriguez <mandorodriguez@gmail.com>
2010-12-21 20:59:37 +0000

    1. Added another exception to the error module. 2. Added more managing methods to the heccer class.

--- glue/swig/python/heccer/errors.py
+++ glue/swig/python/heccer/errors.py
@@ -1,7 +1,27 @@
+from exceptions import Exception
 
 
+class AddressError(Exception):
+    """
 
-class AddressError:
+    """
     def __init__(self, cause):
         self.cause = cause
 
+    def __str__(self):
+        return self.cause
+
+
+class HeccerNotAllocatedError(Exception):
+    """
+
+    """
+    def __init__(self,msg):
+        self.msg = msg
+    
+    def __str__(self):
+
+        error_msg = "The Heccer core data struct is not allocated: %s : %s" %
+        (self.msg, self.value)
+        
+        return self.msg
--- glue/swig/python/heccer/heccer.py
+++ glue/swig/python/heccer/heccer.py
@@ -14,18 +14,26 @@ __status__ = "Development"
 import os
 import sys
 
+import errors
+
 try:
 
     import heccer_base
 
-except ImportError:
-    sys.exit("Could not import compiled SWIG heccer_base library")
+except ImportError, e:
+    sys.exit("Could not import compiled SWIG heccer_base library: %s" % e)
 
 
 
+
+#************************* Begin Heccer **************************
 class Heccer:
-    
-    def __init__(self, options):
+
+
+
+#---------------------------------------------------------------------------
+
+    def __init__(self, options=None):
         """!
         @brief 
         """
@@ -34,9 +42,9 @@ class Heccer:
         
         if 'model_source' in options:
             
-            self._core = heccer_base.HeccerNew("heccer constructed from python", None, None, None)
+            self._heccer_core = heccer_base.HeccerNew("heccer constructed from python", None, None, None)
             
-            heccer_base.HeccerConstruct( self._core,
+            heccer_base.HeccerConstruct( self._heccer_core,
                                          options['model_source'],
                                          options['name'],
                                          None,
@@ -45,63 +53,114 @@ class Heccer:
             
             print "model_source not found in the heccer options, cannot construct a heccer"
 
+        if isinstance(options, heccer_base.HeccerOptions):
+
+            self._options_core = options
+
+        else:
+
+            self._options_core = heccer_base.HeccerOptions()
+
+
+#---------------------------------------------------------------------------
 
     def GetCore(self):
         """!
         @brief Returns the internal raw heccer struct
         """
-        return self._core
+        return self._heccer_core
 
+
+#---------------------------------------------------------------------------
 
     def GetName(self):
         """!
         @brief 
         """
-        if self._core is not None:
+        if self._heccer_core is not None:
             
-            return self._core.pcName
+            return self._heccer_core.pcName
 
         else:
 
-            return None
+            from errors.import HeccerNotAllocatedError
 
+            raise HeccerNotAllocatedError()
+
+
+#---------------------------------------------------------------------------
 
     def GetErrorCount(self):
         """!
         @brief 
         """
-        if self._core is not None:
+        if self._heccer_core is not None:
             
-            return self._core.iErrorCount
+            return self._heccer_core.iErrorCount
 
         else:
 
-            return None
+            from errors.import HeccerNotAllocatedError
+
+            raise HeccerNotAllocatedError()
+
+
+#---------------------------------------------------------------------------
 
     def GetStatus(self):
         """!
         @brief 
         """
-        if self._core is not None:
+        if self._heccer_core is not None:
             
-            return self._core.iStatus
+            return self._heccer_core.iStatus
 
         else:
 
-            return None
+            from errors.import HeccerNotAllocatedError
+
+            raise HeccerNotAllocatedError()
         
+
+#---------------------------------------------------------------------------
 
     def GetAddress(self, serial, field):
         """!
         @brief Returns the Heccer Address variable
         """
-        return heccer_base.HeccerAddressVariable(self._core, serial, field)
+        address = None
         
+        if self._heccer_core is None:
+
+
+            from errors.import HeccerNotAllocatedError
+
+            raise HeccerNotAllocatedError()
+
+        else:
+            
+            address = heccer_base.HeccerAddressVariable(self._heccer_core, serial, field)
+
+        
+        return address
+
+
+
+    def SetIntervalStart(self, dstart):
+
+        
+
+#---------------------------------------------------------------------------
+
     def Step(self, time):
         """!
         @brief
         """
-        return heccer_base.HeccerHeccs(self._core, time)
+
+        return heccer_base.HeccerHeccs(self._heccer_core, time)
+
+
+#---------------------------------------------------------------------------
 
     def Advance(self,time):
         """!
@@ -109,15 +168,28 @@ class Heccer:
         @sa Step
         """
         return self.Step(time)
-        
+
+
+#---------------------------------------------------------------------------
 
     def Compile(self):
         """!
-        @brief 
+        @brief This compiles the solver for the Heccer core
         """
-        heccer_base.HeccerCompileP1(self._core)
-        heccer_base.HeccerCompileP2(self._core)
-        heccer_base.HeccerCompileP3(self._core)
+
+        if self._heccer_core is not None:
+            
+            heccer_base.HeccerCompileP1(self._heccer_core)
+            heccer_base.HeccerCompileP2(self._heccer_core)
+            heccer_base.HeccerCompileP3(self._heccer_core)
+
+        else:
+
+            from errors.import HeccerNotAllocatedError
+
+            raise HeccerNotAllocatedError()
+        
+#---------------------------------------------------------------------------
 
     def Finish(self):
         """!
@@ -125,10 +197,36 @@ class Heccer:
         """
         pass
 
-    def TimeStep(self, dt):
+#---------------------------------------------------------------------------
+
+    def SetTimeStep(self, dt):
         """!
         @brief 
         """
-        self._core.dStep = dt
+        if self._heccer_core is not None:
         
+            self._heccer_core.dStep = dt
 
+        else:
+
+            from errors.import HeccerNotAllocatedError
+        
+            raise HeccerNotAllocatedError()
+    
+#---------------------------------------------------------------------------
+
+    def GetTimeStep(self):
+
+        if self._heccer_core is not None:
+        
+            return self._heccer_core.dStep 
+
+        else:
+
+            from errors.import HeccerNotAllocatedError
+
+            raise HeccerNotAllocatedError()
+
+
+        
+#************************* End Heccer **************************
