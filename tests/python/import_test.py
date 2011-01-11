ff7daf8f84879959e6e75eb0d5c370a18132ce7b
mandorodriguez <mandorodriguez@gmail.com>
2011-01-11 16:33:20 +0000

    1. Readded the import test script with the proper permissions set.

---
+++ tests/python/import_test.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+"""
+This test checks for the for the heccer modules 
+to be present and importable.
+"""
+import os
+import sys
+import pdb
+
+from test_library import add_package_path
+
+add_package_path("heccer")
+
+print "Importing heccer_base"
+import heccer.heccer_base
+
+print "Importing heccer"
+import heccer
+
+print "Done!"
