67647bb85844185c461bb2ebb35726d03317b248
mandorodriguez <mandorodriguez@gmail.com>
2011-04-01 22:53:12 +0000

    1. Fixed an error that caused the wrong time step value to be used during a simulation run.
    2. Added script to test the perfect clamp 10 compartment passive test.
    
    3. Added test files for testing perfect clamp scripts.

--- tests/python/perfectclamp-passive10.py
+++ tests/python/perfectclamp-passive10.py
@@ -23,20 +23,142 @@ from heccer.heccer_base import HECCER_DUMP_VM_MECHANISM_OPERATIONS
 
 HECCER_TESTED_THINGS=1245184
 
+
+compartments = []
+
 c1 = Compartment()
 
 c1.dCm = 4.57537e-11
 c1.dEm = -0.08
 c1.dInitVm = -0.068
-c1.dInject = 0
-c1.dRa = 360502
-c1.dRm = 3.58441e+08
+c1.dInject = 1e-8
+c1.dRa = 360501.9688
+c1.dRm = 358440928
 c1.iParent = -1
 
-intermediary = Intermediary([c1], [0, -1])
+compartments.append(c1)
+
+c2 = Compartment()
+
+c2.dCm = 5.755329373e-12
+c2.dEm = -0.08
+c2.dInitVm = -0.068
+c2.dInject = 0
+c2.dRa = 772813.4375
+c2.dRm = 8548598272.0
+c2.iParent = 0
+
+compartments.append(c2)
+
+c3 = Compartment()
+
+c3.dCm = 9.952665428e-12
+c3.dEm = -0.08
+c3.dInitVm = -0.068
+c3.dInject = 0
+c3.dRa = 1107083.25
+c3.dRm = 4943399424.0
+c3.iParent = 1
+
+compartments.append(c3)
+
+
+c4 = Compartment()
+
+c4.dCm = 3.048169139e-12
+c4.dEm = -0.08
+c4.dInitVm = -0.068
+c4.dInject = 0
+c4.dRa = 306646.9688
+c4.dRm = 1.614083584e+10
+c4.iParent = 2
+
+compartments.append(c4)
+
+
+
+c5 = Compartment()
+
+c5.dCm = 1.866796431e-12
+c5.dEm = -0.08
+c5.dInitVm = -0.068
+c5.dInject = 0
+c5.dRa = 147150.0781
+c5.dRm = 2.635531059e+10
+c5.iParent = 3
+
+compartments.append(c5)
+
+
+c6 = Compartment()
+
+c6.dCm = 3.81543408e-12
+c6.dEm = -0.08
+c6.dInitVm = -0.068
+c6.dInject = 0
+c6.dRa = 335501.9375
+c6.dRm = 1.289499341e+10
+c6.iParent = 4
+
+compartments.append(c6)
+
+
+c7 = Compartment()
+
+c7.dCm = 2.239454065e-12
+c7.dEm = -0.08
+c7.dInitVm = -0.068
+c7.dInject = 0
+c7.dRa = 230128.9531
+c7.dRm = 2.196964147e+10
+c7.iParent = 5
+
+compartments.append(c7)
+
+
+c8 = Compartment()
+
+c8.dCm = 1.754059463e-12
+c8.dEm = -0.08
+c8.dInitVm = -0.068
+c8.dInject = 0
+c8.dRa = 169781.8594
+c8.dRm = 2.804921958e+10
+c8.iParent = 6
+
+compartments.append(c8)
+
+
+c9 = Compartment()
+
+c9.dCm = 3.37096605e-12
+c9.dEm = -0.08
+c9.dInitVm = -0.068
+c9.dInject = 0
+c9.dRa = 442254.125
+c9.dRm = 1.459522355e+10
+c9.iParent = 7
+
+compartments.append(c9)
+
+c10 = Compartment()
+
+c10.dCm = 5.048512389e-12
+c10.dEm = -0.08
+c10.dInitVm = -0.068
+c10.dInject = 0
+c10.dRa = 518791.0938
+c10.dRm = 9745444864.0
+c10.iParent = 8
+
+compartments.append(c10)
+
+
+intermediary = Intermediary(compartments)
 
 
 my_heccer = Heccer(name="unnamed test", pinter=intermediary)
+
 
 from test_library import INTERVAL_START
 from test_library import INTERVAL_END
@@ -45,6 +167,7 @@ from test_library import INTERPOL_INTERVAL_ENTRIES
 from test_library import OPTIONS
 from test_library import STEPS
 from test_library import TIME_GRANULARITY
+
 
 TIME_STEP=3e-6
 STEPS=1000
--- tests/specifications/python/perfectclamp.t
+++ tests/specifications/python/perfectclamp.t
@@ -22,9 +22,24 @@ my $test
 				description => "perfectclamp integration functionality, single passive compartment",
 			       },
 
+			       {
+				arguments => [
+					     ],
+				command => 'tests/python/perfectclamp-passive10.py',
+				command_tests => [
+						  {
+						   description => "Can ten passive compartments be clamped in python ?",
+						   read => (join '', `cat $::config->{core_directory}/tests/specifications/strings/python/perfectclamp-passive10.txt`),
+						   timeout => 100,
+						  },
+						 ],
+				comment => "This is an integration test for perfectclamp and Heccer (python)",
+				description => "perfectclamp integration functionality, ten passive compartments (python)",
+			       },
+
 			      ],
        description => "perfectclamp circuitry (Python)",
-       name => 'perfectclamp.t',
+       name => 'perfectclamp.t (python)',
       };
 
 
