74074d54181ef3796452c4e9bd7e99b8163221de
mandorodriguez+Ubuntu.VM.Home.i686 <mandorodriguez+Ubuntu.VM.Home.i686@gmail.com>
2011-06-30 06:41:07 +0000

    1. Updated library import method for python tests.

--- tests/python/test_library.py
+++ tests/python/test_library.py
@@ -22,19 +22,61 @@ STEPS=10
 TIME_GRANULARITY=1e-9
 TIME_STEP=2e-5
 
+# This is borrowed from django's setup tools
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
 def add_package_path(package):
     """
     Adds an import path to a python module in a project directory.
     """
-    directory = "/neurospaces_project/" + package + "/source/snapshots/0"
 
-    root_path = os.environ['HOME'] + directory
+    path = os.path.join(os.environ['HOME'],
+                        'neurospaces_project',
+                        package,
+                        'source',
+                        'snapshots',
+                        '0',
+                        'glue',
+                        'swig',
+                        'python')
 
-    path = root_path + "/glue/swig/python/neurospaces"
 
+    build_dir = os.path.join(path, 'build')
+
+    python_build = ""
+    parts = []
+    
+    if os.path.exists(build_dir):
+
+        for curr_dir, directories, files in os.walk( build_dir ):
+
+            if os.path.isfile( os.path.join( curr_dir, '__cbi__.py' )):
+
+                parts = list(fullsplit(curr_dir))
+
+                # remove the module and neurospaces directory
+                parts.pop()
+
+                python_build = os.path.join(os.sep, os.path.join(*parts))
+                sys.path.append(python_build)
+
+    pdb.set_trace()
+    # Add paths
     sys.path.append(path)
 
 
-    
 
     
