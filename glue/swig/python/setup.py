5401a31e04700f092a7821fc201c6a627e90ee46
mandorodriguez <mandorodriguez@gmail.com>
2011-10-19 20:27:05 +0000

    1. Fixed an error in the python list in setup.py.

--- glue/swig/python/setup.py
+++ glue/swig/python/setup.py
@@ -475,7 +475,7 @@ _include_paths = [_developer_dir,
                   os.path.join('/','usr','local','neurospaces','instrumentor','hierarchy','output','symbols'),
                   os.path.join('/','usr','local','include','neurospaces'),
                   os.path.join('/','usr','local','include'),
-                  os.path.join('/','usr','local','neurospaces','instrumentor')
+                  os.path.join('/','usr','local','neurospaces','instrumentor'),
                   os.path.join('/','usr','local','include','model-container') ]
 
 try:
