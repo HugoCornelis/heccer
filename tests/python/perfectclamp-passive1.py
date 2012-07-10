e22dd79fef1e44f050f19014275dbb81599b8dfc
mandorodriguez+MacPro <mandorodriguez+MacPro@gmail.com>
2012-07-10 02:31:51 +0000

    1. changed the add_package_path to use the top level packages for heccer and experiment instead of a subdirectory. Subdirectories in python imports is now obsolete.

--- tests/python/perfectclamp-passive1.py
+++ tests/python/perfectclamp-passive1.py
@@ -8,8 +8,8 @@ import pdb
 
 from test_library import add_package_path
 
-add_package_path("heccer",subdir='neurospaces')
-add_package_path("experiment", subdir='neurospaces')
+add_package_path("heccer")
+add_package_path("experiment")
 
 from heccer import Heccer
 from heccer import Compartment
--- tests/python/perfectclamp-passive10.py
+++ tests/python/perfectclamp-passive10.py
@@ -8,8 +8,8 @@ import pdb
 
 from test_library import add_package_path
 
-add_package_path("heccer", "neurospaces")
-add_package_path("experiment", "neurospaces")
+add_package_path("heccer")
+add_package_path("experiment")
 
 
 from heccer import Heccer
